73be1a8e16ab08a5cd00838f1d5cf18d1fd7e360
MrPikac <milos.novisad@icloud.com>
2019-07-03 10:05:23 +0200

    sdawdafdsv

---
+++ server.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+const server = new WebSocket.Server({port:3000});
+
+server.on("connection", function(client) {
+    console.log(client + "has connected.");
+
+    client.on('message', function(resulted) {
+        console.log(client + ': coordinates : ' + resulted);
+
+        client.send(resulted);
+    })
+
+    client.on("client", function() {
+        console.log("client has disconnected.");
+    })
+})
